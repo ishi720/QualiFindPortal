93a8fa926bdaa3b351d95249dff98cb62a321266
ishi720 <ishizuka720@gmail.com>
2025-09-29 11:25:08 +0900

    template_toolkitで一覧ページ作成

--- lib/QualiFindPortal.pm
+++ lib/QualiFindPortal.pm
@@ -3,8 +3,16 @@ use Dancer2;
 
 our $VERSION = '0.1';
 
+# 一覧データ TODO:DBから取得
+my @items = (
+    { id => 1, name => '資格1',  price => 120 },
+    { id => 2, name => '資格2',  price => 80  },
+    { id => 3, name => '資格3',  price => 100 },
+);
+
+# ルートに一覧ページ
 get '/' => sub {
-    template 'index' => { 'title' => 'QualiFindPortal' };
+    template 'qualifications.tt', { items => \@items, title => '資格一覧' };
 };
 
 true;
